63d16ad60ac66bb9da2eeac111fae96bf1c227b1
rnfltpgus <jpp901220@gmail.com>
2021-09-23 00:29:01 +0900

    [UPD] Telegram 단톡변경

--- TelegramAPI.py
+++ TelegramAPI.py
@@ -2,15 +2,18 @@ import telegram
 
 class TelegramAPI:
     def __init__(self):
-        self.APICODE = "1404093576:AAFuWnDfOa2sHfVf_9P5wdHzmB1btsSI_Q8"  #janny_bot
-        #self.BOTID = "1431053678"    #janny_bot
-        self.BOTID = "-469351976"    #주식 - 그룹채팅방ID
+        self.APICODE = "1920006088:AAGLQLxo0RlQQtK-K3LL8UO3GPcmiCHrwSA"  #DANTA_BOT
+        self.BOTID = "-449307718"    #DANTA_BOT - 그룹채팅방ID
+        #self.BOTID = "1431053678"    #janny_bot(좌니가족있는방)
+        #self.BOTID = "1431521277"    #DANTA_BOT - 그룹채팅방ID(좌니가족있는방)
+
 
     def Tel_GetId(self):
         #텔레그램 봇 아이디 구하기
         chat_token = self.APICODE
         chat = telegram.Bot(token = chat_token)
         updates = chat.getUpdates()
+        
         for u in updates:
             print(u.message['chat']['id'])  ##출력 결과  janny_Bot id
 
